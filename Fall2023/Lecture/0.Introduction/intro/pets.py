ca9dc9f37059389a85d4015ec036c06d06d69bf0
q_tong <q_tong@uncg.edu>
2023-12-02 21:45:52 -0500

    lectures

--- Fall2023/Lecture/0.Introduction/intro/pets.py
+++
@@ -1,9 +0,0 @@
-
-class Pet:
-    def __init__(self, name, species,age):
-        self.name = name
-        self.species = species
-        self.age = age
-        
-    def display(self):
-        print(f"{self.name} is a {self.age}-year-old {self.species}")
